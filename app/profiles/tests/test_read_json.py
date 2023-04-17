3fe41c8d92dba5a6c8aa9476b6e40637e770dd47
Gonçalo Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2023-04-17 19:23:17 +0100

    test: add readJSONfile method tests

---
+++ app/profiles/tests/test_read_json.py
@@ -0,0 +1,55 @@
+import pytest
+from profiles import profiles
+import os
+
+
+def clean_files():
+    if os.path.exists("profiles.json"):
+        os.remove("profiles.json")
+    if os.path.exists("read_json_test.json"):
+        os.remove("read_json_test.json")
+
+
+def test_read_json_invalid_input_type():
+
+    with pytest.raises(TypeError) as e:
+        profiles.readJSONfile(1)
+
+    assert "File must be a string" in str(e.value)
+
+    with pytest.raises(TypeError) as e:
+        profiles.readJSONfile(None)
+
+    assert "File must be a string" in str(e.value)
+
+
+def test_read_json_invalid_input_value():
+
+    with pytest.raises(ValueError) as e:
+        profiles.readJSONfile("")
+
+    assert "File can't be None/Null or empty" in str(e.value)
+
+
+def test_read_json_file_doesnt_exist():
+    
+    clean_files()
+    with pytest.raises(FileNotFoundError) as e:
+        profiles.readJSONfile("read_json_test.json")
+    
+    # assert "File not found" in str(e.value)
+
+
+def test_read_json_permission_error():
+    clean_files()
+    with open("read_json_test.json", "w") as f:
+        f.write("")
+        os.chmod("read_json_test.json", 0o222) # Set write only
+
+    with pytest.raises(PermissionError) as e:
+        profiles.readJSONfile("read_json_test.json")
+
+    # assert "Permission denied" in str(e.value)
+
+    clean_files()
+    
